0ad58b85426af9893ca4a0d97914a9ccfe965169
violateer <1828257089@qq.com>
2021-01-19 19:00:31 +0800

    Canny算子

--- demo5-Canny算子/main.m
+++ demo5-Canny算子/main.m
@@ -2,12 +2,11 @@ close all; % 关闭所有图形窗口
 clear all; % 清楚工作空间所有变量
 clc; % 清空命令行
 
-I = imread('demo-Sobel.jpg');
+I = imread('demo.png');
 I = rgb2gray(I);
 I = im2double(I);
-h = fspecial('laplacian'); % laplacian算子
-J = imfilter(I, h, 'replicate'); % 图像滤波
-K = im2bw(J, 80/255); % 变为二值图像
+J = imnoise(I, 'gaussian', 0, 0.01); % 添加高斯噪声
+[K, thresh] = edge(J, 'canny'); % Canny算子检测边缘
 
 figure;
 subplot(121); imshow(J);
